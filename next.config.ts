253d684998479ac55c55c6b9c596749c1566178a
Frank-Paredes <63618338+Frank-styding@users.noreply.github.com>
2026-03-26 06:21:19 -0500

    add manifest

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,22 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  allowedDevOrigins: [
-    "*", // Intento de permitir todo
-    "localhost:3000", // Local
-    "*.ngrok-free.app", // Comodín para cualquier subdominio de ngrok
-  ],
+  reactStrictMode: true,
+  allowedDevOrigins: ["localhost:3000", "*.ngrok-free.app"],
+  turbopack: {},
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
